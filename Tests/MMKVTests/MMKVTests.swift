47f17d502af1f21f10b6ef433c49cc70930dae17
Kyle <kyle201817146@gmail.com>
2023-07-28 23:05:21 +0800

    Update to v1.3.0

---
+++ Tests/MMKVTests/MMKVTests.swift
@@ -0,0 +1,15 @@
+//
+//  MMKVTests.swift
+//  
+//
+//  Created by Kyle on 2023/7/28.
+//
+
+import XCTest
+import MMKV
+
+final class MMKVTests: XCTestCase {
+    func testExample() throws {
+        let _ = MMKV.default()
+    }
+}
